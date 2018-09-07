bebce29b14091ecf8af55811a7eb872aee8becda
Brandon <brandoncrichie@gmail.com>
2018-09-07 14:14:42 -0700

    commit baseline code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+var config = {
+  apiKey: "AIzaSyAMEn5NgJhXgjjDOTs9L7CEVWiZ-AWsSgs",
+  authDomain: "bloc-chat-ff3d0.firebaseapp.com",
+  databaseURL: "https://bloc-chat-ff3d0.firebaseio.com",
+  projectId: "bloc-chat-ff3d0",
+  storageBucket: "bloc-chat-ff3d0.appspot.com",
+  messagingSenderId: "259293635568"
+};
+firebase.initializeApp(config);
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <RoomList firebase={firebase}/>
       </div>
     );
   }
---
+++ src/components/RoomList.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+
+class RoomList extends Component {
+    constructor(props) {
+        super(props);
+       
+       
+        this.state = {
+            rooms: [],
+            title: "Bloc Chat",
+         }
+        this.roomsRef = this.props.firebase.database().ref('rooms');
+    }
+    
+componentDidMount() {
+    this.roomsRef.on('child_added', snapshot => {
+        const room = snapshot.val();
+        room.key = snapshot.key;
+        this.setState({ rooms: this.state.rooms.concat( room ) });
+        //console.log(snapshot.val());
+    });
+   
+    
+}
+
+
+render() {
+    console.log(this.state.rooms)
+    return (
+            <div className="chatTitle">
+            
+            <h1 id="title"> {this.state.title} </h1>
+            
+        
+</div>
+);
+}
+}
+    
+
+export default RoomList;
